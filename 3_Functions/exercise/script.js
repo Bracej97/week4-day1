0b0fa85260b820274e28163692a64c0495420048
Joe Bracey <joebracey1@gmail.com>
2024-10-08 20:02:22 +0100

    Exercise 3 and challenge complete

--- 3_Functions/exercise/script.js
+++ 3_Functions/exercise/script.js
@@ -1,5 +1,21 @@
 function calculateSum(a, b) {
-  console.log("The sum of the numbers is: " + a + b);
+  console.log("The sum of the numbers is: ", a + b);
 }
 
 calculateSum(5, 10);
+
+function calculator(num1, num2, operation) {
+  if (operation === 'add') {
+    console.log(num1 + num2);
+  } else if (operation === 'subtract') {
+    console.log(num1 - num2);
+  } else if (operation === 'multiply') {
+    console.log(num1 * num2);
+  } else if (operation === 'divide') {
+    console.log(num1 / num2);
+  } else {
+    console.log("Must input 2 numbers and operation must be 'add', 'subtract', 'multiply' or 'divide'")
+  }
+}
+
+calculator(10, 2, 'divide')
